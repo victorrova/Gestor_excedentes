2a1dcf873a629094d10b63d0b4fc21793fa9af83
Victor Romero <victorromerovalle@gmail.com>
2023-10-17 20:01:18 +0200

    aa

--- src/wifi.c
+++ src/wifi.c
@@ -1,81 +1,49 @@
 #include "wifi.h"
-#include <storage.h>
+#include "storage.h"
 #include "event.h"
-
-static EventGroupHandle_t s_wifi_event_group;
-
+#include "esp_mac.h"
 
 
 static int s_retry_num = 0;
-static bool scan = false;
 
 static void event_handler(void* arg, esp_event_base_t event_base,int32_t event_id, void* event_data)
-
 {
-    static int ok = 0;
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        ESP_LOGD(__FUNCTION__,"EVENT WIFI_EVENT_STA_START");
-        if(!scan){esp_wifi_connect();}
-        
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
-     {
-        ESP_LOGD(__FUNCTION__,"EVENT WIFI_EVENT_STA_DISCONNECTED");
-        if(ok == 0){
-            if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) 
-            {
-                esp_wifi_connect();
-                s_retry_num++;
-                vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-                ESP_LOGI(__FUNCTION__, "reintentando conectar %d", s_retry_num);
-            } 
-            else 
-            {
-                WIFIstate = false;
-                conf_wifi.mode = false;
-                memset(&conf_wifi.ssid,0,sizeof(conf_wifi.ssid));
-                memset(&conf_wifi.password,0,sizeof(conf_wifi.password) );
-                //strncpy(conf_wifi.password,"",0);
-                //ESP_ERROR_CHECK(esp_event_post(MACHINE_EVENTS,MACHINE_SAVE,NULL,0,portMAX_DELAY));
-                xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-            }
-        }
-        else
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) 
+    {
+        esp_wifi_connect();
+    } 
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) 
+    {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) 
         {
             esp_wifi_connect();
-            vTaskDelay(1000 * s_retry_num / portTICK_PERIOD_MS);
-            ESP_LOGW(__FUNCTION__, "reintentando conectar %d",s_retry_num);
             s_retry_num++;
-            if(s_retry_num > 1000){
-                WIFIstate = false;
-                xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-            }
+            ESP_LOGI(__FUNCTION__, "retry to connect to the AP");
+        } 
+        else 
+        {
+            xEventGroupSetBits(Bits_events, WIFI_FAIL_BIT);
         }
-        ESP_LOGW(__FUNCTION__,"Imposible conetar con AP");
+        ESP_LOGE(__FUNCTION__,"connect to the AP fail");
     } 
     else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) 
     {
-        ESP_LOGD(__FUNCTION__,"EVENT IP_EVENT_STA_GOT_IP");
         ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(__FUNCTION__, "ip:" IPSTR, IP2STR(&event->ip_info.ip));
-        //strncpy(conf_int_mem.ip,(const char *)&event->ip_info.ip,sizeof(ip4_addr_t));
-        //ESP_ERROR_CHECK(esp_event_post(MACHINE_EVENTS,MACHINE_SAVE,NULL,0,portMAX_DELAY));
+        ESP_LOGI(__FUNCTION__, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
         s_retry_num = 0;
-        ok = 1;
-        WIFIstate = true;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-
-       
+        xEventGroupSetBits(Bits_events, WIFI_CONNECTED_BIT);
     }
-    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-        ESP_LOGI(__FUNCTION__,"EVENT WIFI_EVENT_AP_STACONNECTED");
-        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-        ESP_LOGI(__FUNCTION__, "SoftAP %s Lista, AID=%d",MAC2STR(event->mac), event->aid);
-    } 
-    else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-        ESP_LOGI(__FUNCTION__,"EVENT WIFI_EVENT_AP_STADISCONNECTED");
-        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
-        ESP_LOGI(__FUNCTION__, "Soft Ap  %s eliminada, AID=%d",MAC2STR(event->mac), event->aid);
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_STACONNECTED) 
+    {
+        wifi_event_ap_staconnected_t *event = (wifi_event_ap_staconnected_t *) event_data;
+        ESP_LOGI(__FUNCTION__, "Station "MACSTR" joined, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_STADISCONNECTED) 
+    {
+        wifi_event_ap_stadisconnected_t *event = (wifi_event_ap_stadisconnected_t *) event_data;
+        ESP_LOGI(__FUNCTION__, "Station "MACSTR" left, AID=%d",
+                 MAC2STR(event->mac), event->aid);
     }
 }
 
@@ -90,7 +58,7 @@ static esp_err_t set_dns_server(esp_netif_t *netif, uint32_t addr, esp_netif_dns
     return ESP_OK;
 
 }
-esp_err_t static_ip(esp_netif_t *netif)
+static esp_err_t static_ip(esp_netif_t *netif)
 {
     size_t ip_len = storage_get_size("ip");
     esp_netif_ip_info_t Ip;
@@ -111,7 +79,7 @@ esp_err_t static_ip(esp_netif_t *netif)
             ESP_LOGE(__FUNCTION__, "sin memoria dinamica :(");
             return ESP_FAIL;
         }
-        ESP_ERR_CHECK(storage_load(NVS_TYPE_STR,"ip",ip,&ip_len));
+        ESP_ERROR_CHECK(storage_load(NVS_TYPE_STR,"ip",ip,&ip_len));
         Ip.ip.addr = ipaddr_addr((const char*)ip);
         free(ip);
 
@@ -124,7 +92,7 @@ esp_err_t static_ip(esp_netif_t *netif)
                 ESP_LOGE(__FUNCTION__, "sin memoria dinamica :(");
                 return ESP_FAIL;
             }
-            ESP_ERR_CHECK(storage_load(NVS_TYPE_STR,"netmask",netmask,&netmask_len));
+            ESP_ERROR_CHECK(storage_load(NVS_TYPE_STR,"netmask",netmask,&netmask_len));
             Ip.netmask.addr = ipaddr_addr((const char*)netmask);
             free(netmask);
         }
@@ -137,7 +105,7 @@ esp_err_t static_ip(esp_netif_t *netif)
                 ESP_LOGE(__FUNCTION__, "sin memoria dinamica :(");
                 return ESP_FAIL;
             }
-            ESP_ERR_CHECK(storage_load(NVS_TYPE_STR,"netmask",gateway,&gateway_len));
+            ESP_ERROR_CHECK(storage_load(NVS_TYPE_STR,"netmask",gateway,&gateway_len));
             Ip.gw.addr = ipaddr_addr((const char*)gateway);
             free(gateway);
         }
@@ -157,7 +125,7 @@ esp_err_t static_ip(esp_netif_t *netif)
             ESP_LOGE(__FUNCTION__, "sin memoria dinamica :(");
             return ESP_FAIL;
         }
-        ESP_ERR_CHECK(storage_load(NVS_TYPE_STR,"dns1",dns1,&dns1_len));
+        ESP_ERROR_CHECK(storage_load(NVS_TYPE_STR,"dns1",dns1,&dns1_len));
         ESP_ERROR_CHECK(set_dns_server(netif, ipaddr_addr((const char*)dns1), ESP_NETIF_DNS_MAIN));
         free(dns1);
     }
@@ -170,16 +138,15 @@ esp_err_t static_ip(esp_netif_t *netif)
             ESP_LOGE(__FUNCTION__, "sin memoria dinamica :(");
             return ESP_FAIL;
         }
-        ESP_ERR_CHECK(storage_load(NVS_TYPE_STR,"dns2",dns2,&dns2_len));
+        ESP_ERROR_CHECK(storage_load(NVS_TYPE_STR,"dns2",dns2,&dns2_len));
         ESP_ERROR_CHECK(set_dns_server(netif, ipaddr_addr((const char*)dns2), ESP_NETIF_DNS_MAIN));
         free(dns2);
     }
     return ESP_OK;
 }
 
-esp_err_t wifi_init_sta(void)
+esp_err_t wifi_init_sta(const char *ssid, const char* password)
 {
-
     ESP_ERROR_CHECK(esp_netif_init());
 
     esp_netif_t *net = esp_netif_create_default_wifi_sta();
@@ -202,9 +169,12 @@ esp_err_t wifi_init_sta(void)
 
   
     wifi_config_t wifi_config = {0};
-    strcpy((char *)wifi_config.sta.ssid, conf_wifi.ssid);
-    strcpy((char *)wifi_config.sta.password, conf_wifi.password);
 
+    strcpy((char *)wifi_config.sta.ssid,ssid);
+    if(password != NULL)
+    {
+        strcpy((char *)wifi_config.sta.password, password);
+    }
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
@@ -212,20 +182,17 @@ esp_err_t wifi_init_sta(void)
 
     ESP_LOGI(__FUNCTION__, "wifi_init_sta finished.");
 
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+    EventBits_t bits = xEventGroupWaitBits(Bits_events,
             WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
             pdFALSE,
             pdFALSE,
             portMAX_DELAY);
-
-
     if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(__FUNCTION__, "Conectado a SSID:%s",
-                 conf_wifi.ssid);
+        ESP_LOGI(__FUNCTION__, "Conectado a SSID:%s",ssid);
+
         return ESP_OK;
     } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(__FUNCTION__, "Fallo al conectar a SSID:%s",
-                 conf_wifi.ssid);
+        ESP_LOGI(__FUNCTION__, "Fallo al conectar a SSID:%s",ssid);
         return ESP_FAIL;
     } else {
         ESP_LOGE(__FUNCTION__, "UNEXPECTED EVENT");
@@ -239,7 +206,6 @@ char *wifi_scan(void)
     cJSON *msg;
     json = cJSON_CreateObject();
     msg = cJSON_CreateObject();
-    scan= true;
     uint16_t numero = 10;
     wifi_ap_record_t ap_lista[numero];
     uint16_t ap_count = 0;
@@ -260,7 +226,7 @@ char *wifi_scan(void)
 }
 esp_err_t wifi_init_softap(void)
 {
-    scan = true;
+
     ESP_LOGI(__FUNCTION__,"ESP SOFT_AP");
     ESP_ERROR_CHECK(esp_netif_init());
     esp_netif_t *ap = esp_netif_create_default_wifi_ap();
@@ -294,43 +260,56 @@ esp_err_t wifi_init_softap(void)
              SSID,  CANAL);
     return ESP_OK;
 }
-
-esp_err_t Wifi_start(_wifi conf_wifi)
+esp_err_t Wifi_stop(void)
 {
-    
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
-    if(conf_wifi.mode == true)
-    { //
-        ESP_LOGI(__FUNCTION__, "ESP_WIFI_MODE_STA");
-        esp_err_t de = wifi_init_sta(conf_wifi);
-        if(de == ESP_OK)
-        {
-            return ESP_OK;
-        }
-        else
-        {
-            ESP_ERROR_CHECK(wifi_init_softap());
-        }
-    }
-    else
+    ESP_ERROR_CHECK(esp_wifi_disconnect());
+    ESP_ERROR_CHECK(esp_wifi_stop());
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_NULL));
+    return ESP_OK;
+}
+
+esp_err_t Wifi_start(void)
+{
+    esp_err_t ret;
+    nvs_flash_init();
+    size_t ssid_len = storage_get_size("ssid");
+    if( ssid_len > 0)
     {
-        esp_err_t de = wifi_init_softap();
-        if(de == ESP_OK)
+        char *ssid = malloc(sizeof(char) * ssid_len);
+        if(ssid != NULL)
         {
-            return ESP_OK;
-        }
-        else
-        {
+            ESP_LOGE(__FUNCTION__, "sin memoria dinamica :(");
             return ESP_FAIL;
         }
-        
+        ESP_ERROR_CHECK(storage_load(NVS_TYPE_STR,"ssid",ssid,ssid_len));
+        size_t passwd_len = storage_get_size("password");
+        if( passwd_len > 0)
+        {
+            char *passwd = malloc(sizeof(char) * ssid_len);
+            if(passwd != NULL)
+            {
+                ESP_LOGE(__FUNCTION__, "sin memoria dinamica :(");
+                return ESP_FAIL;
+            }
+            ESP_ERROR_CHECK(storage_load(NVS_TYPE_STR,"password",passwd,passwd_len));
+            ESP_GOTO_ON_ERROR(wifi_init_sta(ssid,passwd),ap,__FUNCTION__,"[ERROR] config mode STA");
+
+        }
+        else
+        {
+            ESP_GOTO_ON_ERROR(wifi_init_sta(ssid,NULL),ap,__FUNCTION__,"[ERROR] config mode STA");
+        }
+
+    
+    
     }
-    return ESP_OK;
+    
+
+ap:
+    ESP_ERROR_CHECK(Wifi_stop());
+    ESP_ERROR_CHECK(wifi_init_softap());
+
+return ESP_OK;   
 }
 
 
